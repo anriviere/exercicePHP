81f881501e3429d26c36efd630175287e4624120
anriviere <56391802+anriviere@users.noreply.github.com>
2020-05-18 11:06:08 +0200

    Add files via upload

---
+++ anaisRiviere/index.php
@@ -0,0 +1,226 @@
+
+<?php include("inc/header.inc.php"); ?>
+<?php
+          $result = $pdo->query("SELECT * FROM profil WHERE deletion_flag = 0 ORDER BY id_profil DESC");
+          $profil = $result->fetch(PDO::FETCH_OBJ)?>
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 class="mb-0"><?php echo $profil->prenom; ?>
+          <span class="text-primary"><?php echo $profil->nom; ?></span>
+        </h1>
+        <div class="subheading mb-5"><?php echo $profil->adresse; ?> · <?php echo $profil->code_postal; ?> · <?php echo $profil->ville; ?> ·
+          <a href="mailto:name@email.com"><?php echo $profil->mail; ?></a>
+        </div>
+        <p class="lead mb-5"><?php echo $profil->presentation; ?></p>
+        <div class="social-icons">
+          <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+      <div class="w-100">
+        <h2 class="mb-5">Experience</h2>
+       
+        
+        <?php
+          $result = $pdo->query("SELECT * FROM experiences WHERE deletion_flag = 0 ORDER BY id_experience DESC");
+          while ($experience = $result->fetch(PDO::FETCH_OBJ)) { ?>
+        
+            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"> 
+             <div class="resume-content">      
+              <h3 class="mb-0"><?php echo $experience->poste; ?></h3>
+              <div class="subheading mb-3"><?php echo $experience->employeur; ?></div>
+              <p><?php echo $experience->texte; ?></p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary"><?php echo $experience->date_debut; ?> - <?php echo $experience->date_fin; ?></span>
+              </div>
+            </div>
+
+          <?php } ?>
+        
+  
+
+        
+        
+        
+
+</div>
+
+        
+
+      
+
+       
+
+</div>
+
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="education">
+      <div class="w-100">
+        <h2 class="mb-5">Education</h2>
+
+
+
+        <?php
+          $result = $pdo->query("SELECT * FROM education WHERE deletion_flag = 0 ORDER BY id_education DESC");
+          while ($education = $result->fetch(PDO::FETCH_OBJ)) { ?>      
+
+              <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="resume-content">
+                  <h3 class="mb-0"><?php echo $education->etablissement_scolaire;?></h3>
+                  <div class="subheading mb-3"><?php echo $education->diplome;?></div>
+                  <div><?php echo $education->commentaires;?></div>
+                </div>
+                <div class="resume-date text-md-right">
+                  <span class="text-primary"><?php echo $education->date_debut;?> - <?php echo $education->date_fin;?></span>
+                </div>
+              </div>
+          <?php } ?>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
+      <div class="w-100">
+        <h2 class="mb-5">Skills</h2>
+
+        <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+        <ul class="list-inline dev-icons">
+          <li class="list-inline-item">
+            <i class="fab fa-html5"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-css3-alt"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-js-square"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-angular"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-react"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-node-js"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-sass"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-less"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-wordpress"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-gulp"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-grunt"></i>
+          </li>
+          <li class="list-inline-item">
+            <i class="fab fa-npm"></i>
+          </li>
+        </ul>
+
+        <div class="subheading mb-3">Workflow</div>
+        <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Mobile-First, Responsive Design</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Cross Browser Testing &amp; Debugging</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Cross Functional Teams</li>
+          <li>
+            <i class="fa-li fa fa-check"></i>
+            Agile Development &amp; Scrum</li>
+        </ul>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
+      <div class="w-100">
+        <h2 class="mb-5">Interests</h2>
+        <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+        <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
+      <div class="w-100">
+        <h2 class="mb-5">Awards &amp; Certifications</h2>
+        <ul class="fa-ul mb-0">
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Google Analytics Certified Developer</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            Mobile Web Specialist - Google Certification</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            1<sup>st</sup>
+            Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            1<sup>st</sup>
+            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            2<sup>nd</sup>
+            Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            1<sup>st</sup>
+            Place - James Buchanan High School - Hackathon 2006</li>
+          <li>
+            <i class="fa-li fa fa-trophy text-warning"></i>
+            3<sup>rd</sup>
+            Place - James Buchanan High School - Hackathon 2005</li>
+        </ul>
+      </div>
+    </section>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/resume.min.js"></script>
+
+</body>
+
+</html>
